c6259b678601d6a5993765182f20595ed0bf0ded
feeling <feeling20222024@gmail.com>
2025-05-29 02:03:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
 const admin = require('firebase-admin');
 
 // 2) تهيئة Firebase Admin
-const serviceAccount = require('./firebase-service-account.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
