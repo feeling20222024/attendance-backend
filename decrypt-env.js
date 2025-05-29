7c6533ef56cfbfe54c448e9c9bdb11a3c6e23ba3
feeling <feeling20222024@gmail.com>
2025-05-29 12:21:38 +0300

    Create decrypt-env.js

---
+++ decrypt-env.js
@@ -0,0 +1,16 @@
+const crypto = require("crypto");
+const fs = require("fs");
+
+const algorithm = "aes-256-cbc";
+const key = Buffer.from(fs.readFileSync("key.txt", "utf8"), "base64");
+
+const encrypted = JSON.parse(fs.readFileSync(".env.enc", "utf8"));
+const iv = Buffer.from(encrypted.iv, "base64");
+const encryptedText = encrypted.data;
+
+const decipher = crypto.createDecipheriv(algorithm, key, iv);
+let decrypted = decipher.update(encryptedText, "base64", "utf8");
+decrypted += decipher.final("utf8");
+
+fs.writeFileSync(".env", decrypted);
+console.log("✅ تم فك التشفير وكتابة .env");
