d008b7c1681d137ed03bb36f7cc9b251256b4b40
feeling <feeling20222024@gmail.com>
2025-05-31 00:24:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // 1) استيراد المكتبات
+require('dotenv').config();
 const express               = require('express');
 const cors                  = require('cors');
 const path                  = require('path');
