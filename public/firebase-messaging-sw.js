c411418a2cee5f8693c19e6723b96de6112bb3dc
feeling <feeling20222024@gmail.com>
2025-06-08 19:17:06 +0300

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,29 +1,19 @@
-// public/firebase-messaging-sw.js
-
+// نسخ مباشر من توجيهات Firebase للـ FCM background
 importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-messaging-compat.js');
 
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyClFXniBltSeJrp3sxS3_bAgbrZPo0vP3Y",
   authDomain: "device-streaming-47cbe934.firebaseapp.com",
   projectId: "device-streaming-47cbe934",
   storageBucket: "device-streaming-47cbe934.appspot.com",
   messagingSenderId: "235398312189",
   appId: "1:235398312189:web:8febe5e63f7b134b808e94"
-};
-
-firebase.initializeApp(firebaseConfig);
+});
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(payload => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
   const { title, body } = payload.notification || {};
-  if (title && body) {
-    self.registration.showNotification(title, {
-      body,
-      // يمكنك إضافة أيقونة أو خيارات إضافية هنا:
-      // icon: '/assets/icon.png'
-    });
-  }
+  self.registration.showNotification(title, { body });
 });
