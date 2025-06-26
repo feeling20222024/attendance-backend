aea6f9543bc5037b81eee45973b2603660bd45af
feeling <feeling20222024@gmail.com>
2025-06-26 19:07:08 +0300

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,21 +3,6 @@
 importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-messaging-compat.js');
 
-firebase.initializeApp({ /* نفس firebaseConfig */ });
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage(payload => {
-  const { title, body } = payload.notification || {};
-  if (title) {
-    self.registration.showNotification(title, {
-      body,
-      icon: '/assets/icon.png',
-      vibrate: [100,200,100],
-      tag: payload.notification.tag || undefined
-    });
-  }
-});
-
 // 1) إعداد Firebase
 firebase.initializeApp({
   apiKey:    "AIzaSyClFXniBltSeJrp3sxS3_bAgbrZPo0vP3Y",
@@ -36,11 +21,5 @@ self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
 // 3) التعامل مع الإشعارات في الخلفية
 messaging.onBackgroundMessage(payload => {
   const { title, body } = payload.notification || {};
-  if (title) {
-    self.registration.showNotification(title, {
-      body,
-      icon: '/assets/icon.png',        // أيقونتك
-      data: payload.data              // تمرير بيانات إضافية إذا احتجتها
-    });
-  }
+  self.registration.showNotification(title, { body });
 });
