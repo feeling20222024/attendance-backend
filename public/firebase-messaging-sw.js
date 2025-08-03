ce85d02bad672492e1ae33d9aad48e929528a481
feeling <feeling20222024@gmail.com>
2025-08-03 09:24:22 +0300

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,33 +1,25 @@
 // public/firebase-messaging-sw.js
+
 importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey:           "AIzaSyClFXniBltSeJrp3sxS3_bAgbrZPo0vP3Y",
-  authDomain:       "device-streaming-47cbe934.firebaseapp.com",
-  projectId:        "device-streaming-47cbe934",
-  storageBucket:    "device-streaming-47cbe934.appspot.com",
-  messagingSenderId:"235398312189",
-  appId:            "1:235398312189:web:8febe5e63f7b134b808e94"
+  apiKey: "AIzaSyClFXniBltSeJrp3sxS3_bAgbrZPo0vP3Y",
+  authDomain: "device-streaming-47cbe934.firebaseapp.com",
+  projectId: "device-streaming-47cbe934",
+  storageBucket: "device-streaming-47cbe934.appspot.com",
+  messagingSenderId: "235398312189",
+  appId: "1:235398312189:web:8febe5e63f7b134b808e94"
 });
 
 const messaging = firebase.messaging();
-self.addEventListener('install',  evt => self.skipWaiting());
-self.addEventListener('activate', evt => evt.waitUntil(self.clients.claim()));
 
-messaging.onBackgroundMessage(payload => {
-  const { title = 'Notification', body = '' } = payload.notification || {};
+// هذه المعالجة للرسائل في الخلفية (background)
+messaging.onBackgroundMessage(function(payload) {
+  const { title = '', body = '' } = payload.notification || {};
   self.registration.showNotification(title, {
     body,
-    icon: '/assets/icon.png',
-    vibrate: [100,200,100],
+    tag: 'default',
     data: payload.data
   });
-  const notification = { title, body, time: new Date().toLocaleString() };
-  self.clients.matchAll({ includeUncontrolled: true, type: 'window' })
-    .then(clients => {
-      for (const client of clients) {
-        client.postMessage({ type: 'ADD_NOTIFICATION', notification });
-      }
-    });
 });
